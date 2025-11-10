a51c8321ed86c97953ead24f4f65acf3796e9bcc
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2025-11-10 11:10:26 +0300

    Split Button to source file

---
+++ src/platform/io/button/Button.cpp
@@ -0,0 +1,96 @@
+#include "platform/io/button/Button.h"
+
+#include <time/mono.h>
+
+namespace platform {
+
+namespace sm {
+
+void ButtonSM::poll(bool engaged, const ButtonSettings& settings) {
+    const auto now = static_cast<uint16_t>(ttime::mono::now().millis());
+    const auto elapsed = now - timer_;
+
+    switch (updateStateClearEvent(engaged)) {
+        case Idle | 0b0:
+            break;
+
+        case Idle | 0b1:
+            timer_ = now;
+            set(PressDebounce, ButtonEvent::Pressed);
+            break;
+
+        case PressDebounce | 0b0:
+        case PressDebounce | 0b1:
+            if (elapsed >= settings.debounce_ms) {
+                set(Pressing, ButtonEvent::None);
+            }
+            break;
+
+        case Pressing | 0b0:
+            ++clicks_;
+            timer_ = now;
+            set(ReleaseDebounce, ButtonEvent::Clicked);
+            break;
+
+        case Pressing | 0b1:
+            if (elapsed >= settings.hold_ms) {
+                set(Holding, ButtonEvent::HoldStarted);
+            }
+            break;
+
+        case Holding | 0b0:
+            clicks_ = 0;
+            timer_ = now;
+            set(ReleaseDebounce, ButtonEvent::HoldReleased);
+            break;
+
+        case Holding | 0b1:
+            break;
+
+        case ReleaseDebounce | 0b0:
+            if (elapsed >= settings.debounce_ms) {
+                timer_ = now;
+                set(Waiting, ButtonEvent::None);
+            }
+            break;
+
+        case ReleaseDebounce | 0b1:
+            if (elapsed >= settings.debounce_ms) {
+                timer_ = now;
+                set(PressDebounce, ButtonEvent::Pressed);
+            }
+            break;
+
+        case Waiting | 0b0:
+            if (elapsed >= settings.timeout_ms) {
+                clicks_ = 0;
+                set(Idle, ButtonEvent::Timeout);
+            }
+            break;
+
+        case Waiting | 0b1:
+            timer_ = now;
+            set(PressDebounce, ButtonEvent::Pressed);
+            break;
+
+        default:
+            break;
+    }
+}
+
+SUPP_INLINE void ButtonSM::clearEvent() {
+    state_ = State(state_ & ~EventMask);
+}
+
+SUPP_INLINE void ButtonSM::set(State state, ButtonEvent event) {
+    state_ = State(static_cast<uint8_t>(event) | state);
+}
+
+SUPP_INLINE uint8_t ButtonSM::updateStateClearEvent(bool engaged) {
+    state_ = State((state_ & StateMask) | engaged | ((state_ & ISRMask) == ISRMask));
+    return state_;
+}
+
+}  // namespace sm
+
+}  // namespace platform
--- src/platform/io/button/Button.h
+++ src/platform/io/button/Button.h
@@ -5,8 +5,7 @@
 #include "platform/hal/pin/type.h"
 #include "platform/io/button/ButtonLevel.h"
 
-#include <supp/Flags.h>
-#include <time/mono.h>
+#include <supp/inline.h>
 
 namespace platform {
 
@@ -18,7 +17,7 @@ struct ButtonSettings {
 };
 
 // clang-format off
-enum class ButtonEvent : uint8_t {  // 3 bits
+enum class [[nodiscard]] ButtonEvent : uint8_t {  // 3 bits
     //             0b.XXX....
     None         = 0b00000000,
     Pressed      = 0b00010000,
@@ -51,8 +50,25 @@ constexpr const char* toString(ButtonEvent event) {
 namespace sm {
 
 struct ButtonSM {
-    // The last bit is used for the pin digital value (0/1)
+ public:
+    void poll(bool engaged, const ButtonSettings& settings);
+
+    // The number of clicks preceding the event
+    uint8_t clicks() const { return clicks_; }
+
+    // The event that happened (reset on tick())
+    ButtonEvent event() { return static_cast<ButtonEvent>(state_ & EventMask); }
+
+ protected:
+    SUPP_INLINE void isr() { state_ = State(state_ | ISRMask); }
+
+ private:
     // clang-format off
+    static constexpr uint8_t ISRMask   = 0b10000000;
+    static constexpr uint8_t EventMask = 0b01110000;
+    static constexpr uint8_t StateMask = 0b00001110;
+    static constexpr uint8_t PinMask   = 0b00000001;
+
     enum State : uint8_t {
         //                0b....XXX.
         Idle            = 0b00000010,
@@ -64,100 +80,9 @@ struct ButtonSM {
     };
     // clang-format on
 
-    void poll(bool engaged, const ButtonSettings& settings) {
-        const auto now = static_cast<uint16_t>(ttime::mono::now().millis());
-        const auto elapsed = now - timer_;
-
-        switch (updateStateClearEvent(engaged)) {
-            case Idle | 0b0:
-                break;
-
-            case Idle | 0b1:
-                timer_ = now;
-                set(PressDebounce, ButtonEvent::Pressed);
-                break;
-
-            case PressDebounce | 0b0:
-            case PressDebounce | 0b1:
-                if (elapsed >= settings.debounce_ms) {
-                    set(Pressing, ButtonEvent::None);
-                }
-                break;
-
-            case Pressing | 0b0:
-                ++clicks_;
-                timer_ = now;
-                set(ReleaseDebounce, ButtonEvent::Clicked);
-                break;
-
-            case Pressing | 0b1:
-                if (elapsed >= settings.hold_ms) {
-                    set(Holding, ButtonEvent::HoldStarted);
-                }
-                break;
-
-            case Holding | 0b0:
-                clicks_ = 0;
-                timer_ = now;
-                set(ReleaseDebounce, ButtonEvent::HoldReleased);
-                break;
-
-            case Holding | 0b1:
-                break;
-
-            case ReleaseDebounce | 0b0:
-                if (elapsed >= settings.debounce_ms) {
-                    timer_ = now;
-                    set(Waiting, ButtonEvent::None);
-                }
-                break;
-
-            case ReleaseDebounce | 0b1:
-                if (elapsed >= settings.debounce_ms) {
-                    timer_ = now;
-                    set(PressDebounce, ButtonEvent::Pressed);
-                }
-                break;
-
-            case Waiting | 0b0:
-                if (elapsed >= settings.timeout_ms) {
-                    clicks_ = 0;
-                    set(Idle, ButtonEvent::Timeout);
-                }
-                break;
-
-            case Waiting | 0b1:
-                timer_ = now;
-                set(PressDebounce, ButtonEvent::Pressed);
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    // clang-format off
-    static constexpr uint8_t ISRMask   = 0b10000000;
-    static constexpr uint8_t EventMask = 0b01110000;
-    static constexpr uint8_t StateMask = 0b00001110;
-    static constexpr uint8_t PinMask   = 0b00000001;
-    // clang-format on
-
-    SUPP_INLINE void clearEvent() { state_ = State(state_ & ~EventMask); }
-    SUPP_INLINE void set(State state, ButtonEvent event) {
-        state_ = State(static_cast<uint8_t>(event) | state);
-    }
-
-    SUPP_INLINE uint8_t updateStateClearEvent(bool engaged) {
-        state_ = State((state_ & StateMask) | engaged | ((state_ & ISRMask) == ISRMask));
-        return state_;
-    }
-
-    // The number of clicks preceding the event
-    uint8_t clicks() const { return clicks_; }
-
-    // The event that happened (reset on tick())
-    ButtonEvent event() { return static_cast<ButtonEvent>(state_ & EventMask); }
+    SUPP_INLINE void clearEvent();
+    SUPP_INLINE void set(State state, ButtonEvent event);
+    SUPP_INLINE uint8_t updateStateClearEvent(bool engaged);
 
     uint16_t timer_ = 0;
     uint8_t clicks_ = 0;
@@ -197,10 +122,7 @@ class Button : sm::ButtonSM, public Singleton<Button<Pin, S>> {
     using sm::ButtonSM::event;
 
  private:
-    static void buttonISR() {
-        auto& s = Button::instance().state_;
-        s = State(s | ISRMask);
-    }
+    static void buttonISR() { Button::instance().isr(); }
 
     CallbackType* callback_ = nullptr;
     [[no_unique_address]] const Pin pin{};
